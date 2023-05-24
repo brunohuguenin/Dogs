efaafe7ea88db408530d297e1c5bb96399e25c6e
Brunohugueninn <brunohugueninn@gmail.com>
2023-05-24 19:19:37 -0300

    estilização da home

--- dogs/src/Components/login/Login.jsx
+++ dogs/src/Components/login/Login.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import LoginForm from './LoginForm'
+import LoginCreate from './LoginCreate'
+import LoginPasswordLost from './LoginPasswordLost'
+import LoginPasswordReset from './LoginPasswordReset'
 
 const Login = () => {
   return (
     <div>
-      Login
+      <Routes>
+      </Routes>
+        <Route path='/' element={<LoginForm />} />
+        <Route path='/' element={<LoginCreate />} /> 
+        <Route path='/' element={<LoginPasswordLost/>} /> 
+        <Route path='/' element={<LoginPasswordReset/>} /> 
     </div>
   )
 }
