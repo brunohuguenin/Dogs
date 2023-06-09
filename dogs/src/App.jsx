a5f644004701e338f229964661ca247105dc5e1b
Brunohugueninn <brunohugueninn@gmail.com>
2023-06-09 17:34:44 -0300

    perfil do usuário criado

--- dogs/src/App.jsx
+++ dogs/src/App.jsx
@@ -9,6 +9,7 @@ import { UserStorage } from './UserContext';
 import User from './Components/User/User';
 import ProtectedRouter from './Components/Helper/ProtectedRouter';
 import Photo from './Components/Photo/Photo';
+import UserProfile from './Components/User/UserProfile';
 
 
 
@@ -25,6 +26,7 @@ const App = () => {
               <User />
             </ProtectedRouter>}/>
             <Route path='foto/:id' element={<Photo/>}/>
+            <Route path='perfil/:user' element={<UserProfile/>}/>
           </Routes>
           <Footer />
         </UserStorage>
