4bc4b195f21d0ee79c17f6910c6434b8445d852f
Brunohugueninn <brunohugueninn@gmail.com>
2023-06-09 22:04:05 -0300

    perdeu a senha criado

--- dogs/src/Components/login/LoginPasswordLost.jsx
+++ dogs/src/Components/login/LoginPasswordLost.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import Input from '../Forms/Input'
+import Button from '../Forms/Button'
+import useForm from '../../Hooks/useForm'
+import UseFetch from '../../Hooks/UseFetch'
+import { PASSWORD_LOST } from '../../api'
+import Error from '../Helper/Error'
+
 
 const LoginPasswordLost = () => {
+  const login = useForm();
+  const {data, loading, error, request} = UseFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (login.validate()) {
+      const {url, options} = PASSWORD_LOST({login: login.value, url: window.location.href.replace('perdeu', 'resetar')});
+      const {json} =  await request(url, options)
+    }
+  }
+
+
   return (
-    <div>
+    <section>
+      <h1 className='title'>Perdeu a senha?</h1>
+      {data ? <p style={{color: '#4c1'}}>{data}</p> : <form onSubmit={handleSubmit}>
+        <Input label='Email / Usuário' type='text' name='email' {...login}/>
+        {loading ? <Button>Enviando...</Button> : <Button>Enviar Email</Button>}
+      </form>}
       
-    </div>
+      <Error error={error} />
+    </section>
   )
 }
 
